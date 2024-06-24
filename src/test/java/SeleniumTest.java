d6d0c03c1f3bd4943568c986350d2b55fdd35edf
jmblancor1 <jm.blancor1@uniandes.edu.co>
2024-06-24 17:05:32 -0500

    Initial commit

---
+++ src/test/java/SeleniumTest.java
@@ -0,0 +1,30 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+    WebDriver driver = new ChromeDriver();
+    @BeforeTest
+    void Setup(){
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\src\\test\\resources\\drivers\\chrome\\version_115\\chromedriver.exe");
+
+
+        driver.get("http://www.testingyes.com/onlineshop/");
+
+    }
+    @AfterTest
+    void TearDown(){
+
+        driver.close();
+        driver.quit();
+    }
+    @Test
+    void test(){
+
+        String title = driver.getTitle();
+        System.out.println(title);
+
+    }
+}
